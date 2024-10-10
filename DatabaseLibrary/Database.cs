875fb9e29446ed8bd1164794e7896014741dc5d5
DigitalJSon <pardinasjaysonb@gmail.com>
2024-10-10 11:55:18 +0800

    created project for Database functions and added function to create database if not yet created

---
+++ DatabaseLibrary/Database.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SQLite;
+namespace DatabaseLibrary
+{
+    public class Database
+    {
+        public SQLiteConnection Connection;
+
+        private static readonly string databaseName = "database";
+
+        public string connectionString {  get; set; }
+
+        string connection;
+
+        public void GetConnection()
+        {
+            connection = @$"Data Source={databaseName}.db; Version=3";
+            connectionString = connection;
+        }
+
+        public Database()
+        {
+            if (!File.Exists($"./{databaseName}.db"))
+            {
+                Console.WriteLine("File not found. Creating database...");
+                SQLiteConnection.CreateFile($"{databaseName}.db");
+                GetConnection();
+
+                using (SQLiteConnection con = new SQLiteConnection(connection))
+                {
+                    SQLiteCommand command = new SQLiteCommand();
+                    con.Open();
+
+                    string query = @"CREATE TABLE played_videogames(id INTEGER PRIMARY KEY AUTOINCREMENT, hours_played INTEGER, data TEXT)";
+                    command.CommandText = query;
+                    command.Connection = con;
+                    command.ExecuteNonQuery();
+
+                    con.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Database found!");
+            }
+
+        }
+    }
+}
--- HabitTracker.DigitalJson/Program.cs
+++ HabitTracker.DigitalJson/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DatabaseLibrary;
+
+namespace HabitTrackerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+        }
+    }
+}

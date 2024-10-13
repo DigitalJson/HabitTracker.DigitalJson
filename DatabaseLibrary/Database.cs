6cb02050c4e104f13458be2b9b91f596f206d11e
DigitalJSon <pardinasjaysonb@gmail.com>
2024-10-13 11:20:04 +0800

    added function to read all records and insert new ones

--- DatabaseLibrary/Database.cs
+++ DatabaseLibrary/Database.cs
@@ -1,27 +1,28 @@
 ﻿using System.Data.SQLite;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace DatabaseLibrary
 {
+
+    public class PlayedVideoGames
+    {
+        public int Id { get; set; }
+        public int HoursPlayed { get; set; }
+        public DateTime Date { get; set; }
+    }
     public class Database
     {
         public SQLiteConnection Connection;
 
         private static readonly string databaseName = "database";
 
-        public string connectionString {  get; set; }
-
         string connection;
 
-        public void GetConnection()
-        {
-            connection = @$"Data Source={databaseName}.db; Version=3";
-            connectionString = connection;
-        }
-
+        List<PlayedVideoGames> dbData;
         public Database()
         {
             if (!File.Exists($"./{databaseName}.db"))
             {
-                Console.WriteLine("File not found. Creating database...");
                 SQLiteConnection.CreateFile($"{databaseName}.db");
                 GetConnection();
 
@@ -30,7 +31,7 @@ namespace DatabaseLibrary
                     SQLiteCommand command = new SQLiteCommand();
                     con.Open();
 
-                    string query = @"CREATE TABLE played_videogames(id INTEGER PRIMARY KEY AUTOINCREMENT, hours_played INTEGER, data TEXT)";
+                    string query = @"CREATE TABLE played_videogames(id INTEGER PRIMARY KEY AUTOINCREMENT, hours_played INTEGER, date TEXT)";
                     command.CommandText = query;
                     command.Connection = con;
                     command.ExecuteNonQuery();
@@ -38,11 +39,125 @@ namespace DatabaseLibrary
                     con.Close();
                 }
             }
-            else
-            {
-                Console.WriteLine("Database found!");
-            }
 
         }
+
+        private int GetHoursPlayed()
+        {
+            string? input = "";
+            int hoursPlayed = 0;
+
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out hoursPlayed) || hoursPlayed < 1)
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+                input = Console.ReadLine();
+            }
+            return hoursPlayed;
+        }
+
+        private string GetDatePlayed()
+        {
+            string? input = "";
+            string date;
+
+            input = Console.ReadLine();
+            while (!DateTime.TryParseExact(input, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None,out _))
+            {
+                Console.WriteLine("Invalid date input. Please input the date in this format 'MM/dd/yyyy");
+                input = Console.ReadLine();
+            }
+            date = input;
+            return date;
+        }
+        public void InsertNewRecord()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter how long you have played.");
+            int hoursPlayedInput = GetHoursPlayed();
+
+            Console.WriteLine("Please enter the date. Enter in this format 'MM/dd/yyyy'.");
+            string dateInput = GetDatePlayed();
+
+            Add(hoursPlayedInput, dateInput);
+
+            Console.WriteLine("Record successfully added");
+        }
+        private void Add(int hoursPlayed, string date)
+        {
+            GetConnection();
+
+            using (SQLiteConnection con = new SQLiteConnection(connection))
+            {
+                SQLiteCommand command = new SQLiteCommand();
+                con.Open();
+
+                string query = $@"INSERT INTO played_videogames (hours_played, date)
+                                VALUES ({hoursPlayed},'{date}')";
+                command.CommandText = query;
+                command.Connection = con;
+                command.ExecuteNonQuery();
+
+                con.Close();
+            }
+        }
+        public void ReadAllRecords()
+        {
+            GetConnection();
+
+            using (SQLiteConnection con = new SQLiteConnection(connection))
+            {
+                SQLiteCommand command = new SQLiteCommand();
+                con.Open();
+
+                string query = @"SELECT * FROM played_videogames";
+                command.CommandText = query;
+                command.Connection = con;
+
+                SQLiteDataReader reader = command.ExecuteReader();
+                dbData = new List<PlayedVideoGames>();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        dbData.Add(
+                            new PlayedVideoGames()
+                            {
+                                Id = reader.GetInt32(0),
+                                HoursPlayed = reader.GetInt32(1),
+                                Date = DateTime.ParseExact(reader.GetString(2), "MM/dd/yyyy", new CultureInfo("en-US"))
+                            });
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                con.Close();
+            }
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("ID\t\thours played\t\tdate");
+            for (int i = 0; i < dbData.Count; i++)
+            {
+                Console.WriteLine($"{dbData[i].Id}\t\t{dbData[i].HoursPlayed}\t\t\t{dbData[i].Date.ToString("MM/dd/yyyy")}");
+                if (i < dbData.Count - 1)
+                {
+                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                }
+            }
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void GetConnection()
+        {
+            connection = @$"Data Source={databaseName}.db; Version=3";
+        }
+
+        
+
     }
 }
--- HabitTracker.DigitalJson/Program.cs
+++ HabitTracker.DigitalJson/Program.cs
@@ -7,6 +7,50 @@ namespace HabitTrackerProgram
         static void Main(string[] args)
         {
             Database database = new Database();
+            bool endApp = false;
+            string? input = "";
+            int choice = -1;
+
+            while (!endApp)
+            {
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Type the associated number with what you wish to do. Type 0 if you wish to exit.");
+                Console.WriteLine("1 - View All Records");
+                Console.WriteLine("2 - Add New Record");
+                Console.WriteLine("3 - Update Record");
+                Console.WriteLine("4 - Delete Record");
+
+                input = Console.ReadLine();
+                while (!int.TryParse(input, out choice) || (choice > 4 || choice < 0))
+                {
+                    Console.WriteLine("Invalid input. Please type the associated number of what you wish to do.");
+                    input = Console.ReadLine();
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        // View all records
+                        database.ReadAllRecords();
+                        break;
+                    case 2:
+                        // Add new record
+                        database.InsertNewRecord();
+                        break;
+                    case 3:
+                        // Update record
+                        break;
+                    case 4:
+                        // Delete record
+                        break;
+                    case 0:
+                        Console.WriteLine("Closing app...");
+                        endApp = true;
+                        break;
+                }
+            }
+            
         }
     }
 }

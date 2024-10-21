ba6bfa5aef322deec7e12fcd610b84474e9c3f97
DigitalJSon <pardinasjaysonb@gmail.com>
2024-10-21 10:46:50 +0800

    changed namespace covention and changed habit to tracking steps did in accordance with project requirements

--- DatabaseLibrary/Database.cs
+++ DatabaseLibrary/Database.cs
@@ -1,90 +1,29 @@
 ﻿using System.Data.SQLite;
 using System.Globalization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-namespace DatabaseLibrary
+namespace DatabaseLibrary;
+
+
+public class NumberOfSteps
 {
+    public int Id { get; set; }
+    public int Steps { get; set; }
+    public DateTime Date { get; set; }
+}
+public class Database
+{
+    public SQLiteConnection Connection;
 
-    public class PlayedVideoGames
+    private static readonly string databaseName = "database";
+
+    string connection;
+
+    List<NumberOfSteps> dbData;
+    public Database()
     {
-        public int Id { get; set; }
-        public int HoursPlayed { get; set; }
-        public DateTime DatePlayed { get; set; }
-    }
-    public class Database
-    {
-        public SQLiteConnection Connection;
-
-        private static readonly string databaseName = "database";
-
-        string connection;
-
-        List<PlayedVideoGames> dbData;
-        public Database()
+        if (!File.Exists($"./{databaseName}.db"))
         {
-            if (!File.Exists($"./{databaseName}.db"))
-            {
-                SQLiteConnection.CreateFile($"{databaseName}.db");
-                GetConnection();
-
-                using (SQLiteConnection con = new SQLiteConnection(connection))
-                {
-                    SQLiteCommand command = new SQLiteCommand();
-                    con.Open();
-
-                    string query = @"CREATE TABLE played_videogames(id INTEGER PRIMARY KEY AUTOINCREMENT, hours_played INTEGER, date TEXT)";
-                    command.CommandText = query;
-                    command.Connection = con;
-                    command.ExecuteNonQuery();
-
-                    con.Close();
-                }
-            }
-
-        }
-
-        private int GetHoursPlayed()
-        {
-            string? input = "";
-            int hoursPlayed = 0;
-
-            Console.WriteLine("Please enter how long you have played.");
-            input = Console.ReadLine();
-            while (!int.TryParse(input, out hoursPlayed) || hoursPlayed < 1)
-            {
-                Console.WriteLine("Invalid input. Please try again.");
-                input = Console.ReadLine();
-            }
-            return hoursPlayed;
-        }
-
-        private string GetDatePlayed()
-        {
-            string? input = "";
-            string date;
-
-            Console.WriteLine("Please enter the date. Enter in this format 'MM/dd/yyyy'.");
-            input = Console.ReadLine();
-            while (!DateTime.TryParseExact(input, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None,out _))
-            {
-                Console.WriteLine("Invalid date input. Please input the date in this format 'MM/dd/yyyy");
-                input = Console.ReadLine();
-            }
-            date = input;
-            return date;
-        }
-        public void InsertNewRecord()
-        {
-            Console.Clear();
-            int hoursPlayedInput = GetHoursPlayed();
-
-            string dateInput = GetDatePlayed();
-
-            Add(hoursPlayedInput, dateInput);
-
-            Console.WriteLine("Record successfully added");
-        }
-        private void Add(int hoursPlayed, string date)
-        {
+            SQLiteConnection.CreateFile($"{databaseName}.db");
             GetConnection();
 
             using (SQLiteConnection con = new SQLiteConnection(connection))
@@ -92,8 +31,7 @@ namespace DatabaseLibrary
                 SQLiteCommand command = new SQLiteCommand();
                 con.Open();
 
-                string query = $@"INSERT INTO played_videogames (hours_played, date)
-                                VALUES ({hoursPlayed},'{date}')";
+                string query = @"CREATE TABLE dbTable(id INTEGER PRIMARY KEY AUTOINCREMENT, numberofSteps INTEGER, date TEXT)";
                 command.CommandText = query;
                 command.Connection = con;
                 command.ExecuteNonQuery();
@@ -101,157 +39,217 @@ namespace DatabaseLibrary
                 con.Close();
             }
         }
-        public void ReadAllRecords()
+    }
+
+    private int GetStepCount()
+    {
+        string? input = "";
+        int stepCount = 0;
+
+        Console.WriteLine("Please enter how many steps you've done.");
+        input = Console.ReadLine();
+        while (!int.TryParse(input, out stepCount) || stepCount < 1)
         {
-            GetConnection();
-
-            using (SQLiteConnection con = new SQLiteConnection(connection))
-            {
-                SQLiteCommand command = new SQLiteCommand();
-                con.Open();
-
-                string query = @"SELECT * FROM played_videogames";
-                command.CommandText = query;
-                command.Connection = con;
-
-                SQLiteDataReader reader = command.ExecuteReader();
-                dbData = new List<PlayedVideoGames>();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        dbData.Add(
-                            new PlayedVideoGames()
-                            {
-                                Id = reader.GetInt32(0),
-                                HoursPlayed = reader.GetInt32(1),
-                                DatePlayed = DateTime.ParseExact(reader.GetString(2), "MM/dd/yyyy", new CultureInfo("en-US"))
-                            });
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No rows found.");
-                }
-
-                con.Close();
-            }
-
-            Console.WriteLine("-----------------------------------------------------------------");
-            Console.WriteLine("ID\t\thours played\t\tdate");
-            for (int i = 0; i < dbData.Count; i++)
-            {
-                Console.WriteLine($"{dbData[i].Id}\t\t{dbData[i].HoursPlayed}\t\t\t{dbData[i].DatePlayed.ToString("MM/dd/yyyy")}");
-                if (i < dbData.Count - 1)
-                {
-                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-                }
-            }
-            Console.WriteLine("-----------------------------------------------------------------");
-            Console.ReadKey();
+            Console.WriteLine("Invalid input. Please try again.");
+            input = Console.ReadLine();
         }
+        return stepCount;
+    }
 
-        private int GetID()
+    private string GetDate()
+    {
+        string? input = "";
+        string date;
+
+        Console.WriteLine("Please enter the date. Enter in this format 'MM/dd/yyyy'.");
+        input = Console.ReadLine();
+        while (!DateTime.TryParseExact(input, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None,out _))
         {
-            int id = 0;
-            string? choice = "";
-            ReadAllRecords();
-            Console.WriteLine("Type the id of the record you wish to update.");
-            choice = Console.ReadLine();
-            while (!int.TryParse(choice, out id) || (id > dbData.Count || id < 1))
-            {
-                Console.WriteLine("Input is invalid. Please try again.");
-                choice = Console.ReadLine();
-            }
-            return id;
+            Console.WriteLine("Invalid date input. Please input the date in this format 'MM/dd/yyyy");
+            input = Console.ReadLine();
         }
+        date = input;
+        return date;
+    }
+    public void InsertNewRecord()
+    {
+        Console.Clear();
+        int stepCountInput = GetStepCount();
 
-        private bool DoesIDExist(int id)
+        string dateInput = GetDate();
+
+        Add(stepCountInput, dateInput);
+
+        Console.WriteLine("Record successfully added");
+    }
+    private void Add(int stepCount, string date)
+    {
+        GetConnection();
+
+        using (SQLiteConnection con = new SQLiteConnection(connection))
         {
-            bool recordExists = false;
+            SQLiteCommand command = new SQLiteCommand();
+            con.Open();
 
-            for (int i = 0; i < dbData.Count; i++)
+            string query = $@"INSERT INTO dbTable (numberofSteps, date)
+                                VALUES ({stepCount},'{date}')";
+            command.CommandText = query;
+            command.Connection = con;
+            command.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+    public void ReadAllRecords()
+    {
+        GetConnection();
+
+        using (SQLiteConnection con = new SQLiteConnection(connection))
+        {
+            SQLiteCommand command = new SQLiteCommand();
+            con.Open();
+
+            string query = @"SELECT * FROM dbTable";
+            command.CommandText = query;
+            command.Connection = con;
+
+            SQLiteDataReader reader = command.ExecuteReader();
+            dbData = new List<NumberOfSteps>();
+
+            if (reader.HasRows)
             {
-                if (dbData[i].Id == id)
+                while (reader.Read())
                 {
-                    recordExists = true;
+                    dbData.Add(
+                        new NumberOfSteps()
+                        {
+                            Id = reader.GetInt32(0),
+                            Steps = reader.GetInt32(1),
+                            Date = DateTime.ParseExact(reader.GetString(2), "MM/dd/yyyy", new CultureInfo("en-US"))
+                        });
                 }
-            }
-            if (recordExists)
-            {
-                return true;
             }
             else
             {
-                return false;
+                Console.WriteLine("No rows found.");
             }
+
+            con.Close();
         }
-        public void UpdateRecord()
+
+        Console.WriteLine("-----------------------------------------------------------------");
+        Console.WriteLine("ID\t\tnumber of steps\t\tdate");
+        for (int i = 0; i < dbData.Count; i++)
         {
-            int id = GetID();
-            if (!DoesIDExist(id))
+            Console.WriteLine($"{dbData[i].Id}\t\t{dbData[i].Steps}\t\t\t{dbData[i].Date.ToString("MM/dd/yyyy")}");
+            if (i < dbData.Count - 1)
             {
-                Console.Clear();
-                Console.WriteLine("Record not found. Please try again.");
-                id = GetID();
+                Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
             }
-
-            int hoursPlayed = GetHoursPlayed();
-            string date = GetDatePlayed();
-
-            GetConnection();
-
-            using (SQLiteConnection con = new SQLiteConnection(connection))
-            {
-                SQLiteCommand command = new SQLiteCommand();
-                con.Open();
-
-                string query = $@"UPDATE played_videogames SET hours_played = {hoursPlayed}, date = '{date}' WHERE id = {id}";
-                command.CommandText = query;
-                command.Connection = con;
-                command.ExecuteNonQuery();
-
-                con.Close();
-            }
-            Console.Clear();
-            Console.WriteLine("Record updated successfully\n");
         }
-
-        public void DeleteRecord()
-        {
-
-            int id = GetID();
-            if (!DoesIDExist(id))
-            {
-                Console.Clear();
-                Console.WriteLine("Record not found. Please try again.");
-                id = GetID();
-            }
-            GetConnection();
-
-            using (SQLiteConnection con = new SQLiteConnection(connection))
-            {
-                SQLiteCommand command = new SQLiteCommand();
-                con.Open();
-
-                string query = $@"DELETE FROM played_videogames WHERE id = {id}";
-                command.CommandText = query;
-                command.Connection = con;
-                command.ExecuteNonQuery();
-
-                
-                con.Close();
-            }
-            Console.Clear();
-            Console.WriteLine("Record deleted successfully\n");
-        }
-        public void GetConnection()
-        {
-            connection = @$"Data Source={databaseName}.db; Version=3";
-        }
-
-        
-
+        Console.WriteLine("-----------------------------------------------------------------");
+        Console.ReadKey();
     }
+
+    private int GetID()
+    {
+        int id = 0;
+        string? choice = "";
+        ReadAllRecords();
+        Console.WriteLine("Type the id of the record you wish to update.");
+        choice = Console.ReadLine();
+        while (!int.TryParse(choice, out id) || id < 1)
+        {
+            Console.WriteLine("Input is invalid. Please try again.");
+            choice = Console.ReadLine();
+        }
+        return id;
+    }
+
+    private bool DoesIDExist(int id)
+    {
+        bool recordExists = false;
+
+        for (int i = 0; i < dbData.Count; i++)
+        {
+            if (dbData[i].Id == id)
+            {
+                recordExists = true;
+            }
+        }
+        if (recordExists)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void UpdateRecord()
+    {
+        int id = GetID();
+        while (!DoesIDExist(id))
+        {
+            Console.Clear();
+            Console.WriteLine("Record not found. Please try again.");
+            id = GetID();
+        }
+
+        int stepCount = GetStepCount();
+        string date = GetDate();
+
+        GetConnection();
+
+        using (SQLiteConnection con = new SQLiteConnection(connection))
+        {
+            SQLiteCommand command = new SQLiteCommand();
+            con.Open();
+
+            string query = $@"UPDATE dbTable SET numberofSteps = {stepCount}, date = '{date}' WHERE id = {id}";
+            command.CommandText = query;
+            command.Connection = con;
+            command.ExecuteNonQuery();
+
+            con.Close();
+        }
+        Console.Clear();
+        Console.WriteLine("Record updated successfully\n");
+    }
+
+    public void DeleteRecord()
+    {
+
+        int id = GetID();
+        while (!DoesIDExist(id))
+        {
+            Console.Clear();
+            Console.WriteLine("Record not found. Please try again.");
+            id = GetID();
+        }
+        GetConnection();
+
+        using (SQLiteConnection con = new SQLiteConnection(connection))
+        {
+            SQLiteCommand command = new SQLiteCommand();
+            con.Open();
+
+            string query = $@"DELETE FROM dbTable WHERE id = {id}";
+            command.CommandText = query;
+            command.Connection = con;
+            command.ExecuteNonQuery();
+
+            
+            con.Close();
+        }
+        Console.Clear();
+        Console.WriteLine("Record deleted successfully\n");
+    }
+    public void GetConnection()
+    {
+        connection = @$"Data Source={databaseName}.db; Version=3";
+    }
+
+    
+
 }
--- HabitTracker.DigitalJson/Program.cs
+++ HabitTracker.DigitalJson/Program.cs
@@ -1,59 +1,58 @@
 ﻿using DatabaseLibrary;
 
-namespace HabitTrackerProgram
+namespace HabitTrackerProgram;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Database database = new Database();
+        bool endApp = false;
+        string? input = "";
+        int choice = -1;
+
+        while (!endApp)
         {
-            Database database = new Database();
-            bool endApp = false;
-            string? input = "";
-            int choice = -1;
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Type the associated number with what you wish to do. Type 0 if you wish to exit.");
+            Console.WriteLine("1 - View All Records");
+            Console.WriteLine("2 - Add New Record");
+            Console.WriteLine("3 - Update Record");
+            Console.WriteLine("4 - Delete Record");
 
-            while (!endApp)
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out choice) || (choice > 4 || choice < 0))
             {
-                Console.WriteLine("MAIN MENU");
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("Type the associated number with what you wish to do. Type 0 if you wish to exit.");
-                Console.WriteLine("1 - View All Records");
-                Console.WriteLine("2 - Add New Record");
-                Console.WriteLine("3 - Update Record");
-                Console.WriteLine("4 - Delete Record");
-
+                Console.WriteLine("Invalid input. Please type the associated number of what you wish to do.");
                 input = Console.ReadLine();
-                while (!int.TryParse(input, out choice) || (choice > 4 || choice < 0))
-                {
-                    Console.WriteLine("Invalid input. Please type the associated number of what you wish to do.");
-                    input = Console.ReadLine();
-                }
-
-                switch (choice)
-                {
-                    case 1:
-                        // View all records
-                        database.ReadAllRecords();
-                        Console.Clear();
-                        break;
-                    case 2:
-                        // Add new record
-                        database.InsertNewRecord();
-                        break;
-                    case 3:
-                        // Update record
-                        database.UpdateRecord();
-                        break;
-                    case 4:
-                        // Delete record
-                        database.DeleteRecord();
-                        break;
-                    case 0:
-                        Console.WriteLine("Closing app...");
-                        endApp = true;
-                        break;
-                }
             }
-            
+
+            switch (choice)
+            {
+                case 1:
+                    // View all records
+                    database.ReadAllRecords();
+                    Console.Clear();
+                    break;
+                case 2:
+                    // Add new record
+                    database.InsertNewRecord();
+                    break;
+                case 3:
+                    // Update record
+                    database.UpdateRecord();
+                    break;
+                case 4:
+                    // Delete record
+                    database.DeleteRecord();
+                    break;
+                case 0:
+                    Console.WriteLine("Closing app...");
+                    endApp = true;
+                    break;
+            }
         }
+        
     }
 }
